ff3de856c171df8a6f6576c933485d377a1a523f
Marcos <marcosvasconcelos889@gmail.com>
2025-05-25 00:11:44 -0300

    add página de coleta

---
+++ EcoTech-main/app_doacao_coleta/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def coleta(request):
+    return render(request, 'coleta.html')
--- EcoTech-main/django_log/urls.py
+++ EcoTech-main/django_log/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from app_doacao_coleta import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls')),
     path('', lambda request: redirect('/auth/login/')),
-    path('plataforma/', include('plataforma.urls'))
+    path('plataforma/', include('plataforma.urls')),
+    path('coleta/', views.coleta, name='coleta')
 ]
